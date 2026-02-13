eedebce2ebf0340be9d9209ffb5429c7b649362c
Leslie Lopez <leslie2515lopez@gmail.com>
2026-02-13 15:59:00 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
-import emailjs from "emailjs/nodejs";
+import emailjs from "@emailjs/nodejs";
 import dotenv from "dotenv";
 
 dotenv.config();
